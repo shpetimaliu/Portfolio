3dd3adcaaa40f7f2d281bbd96508254d2a7cf2a4
Shpetim <shpetimmaliu@gmail.com>
2023-11-26 23:37:49 +0100

    up

--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -1,11 +1,11 @@
 "use client";
 import clsx from "clsx";
-import { MotionConfig, useReducedMotion } from "framer-motion";
+import { MotionConfig, motion, useReducedMotion } from "framer-motion";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { useEffect, useId, useRef, useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
-
+import { IoMdClose } from "react-icons/io";
 import Button from "./Button";
 import Container from "./Container";
 import Logo from "./Logo";
@@ -36,7 +36,7 @@ const Header = ({
             aria-expanded={expanded.toString()}
             aria-controls={panelId}
             className={clsx(
-              "group-m-1 rounded-full p-2.5 transition",
+              "group -m-1.5 rounded-full p-2.5 transition",
               invert ? "hover:bg-white/10" : "hover:bg-neutral-950/10"
             )}
             aria-label="Toggle navigation"
@@ -53,6 +53,40 @@ const Header = ({
         </div>
       </div>
     </Container>
+  );
+};
+
+const NavigationRow = ({ children }) => {
+  return (
+    <div className="event:mt-px sm:bg-neutral-950">
+      <Container>
+        <div className="grid grid-cols-2 sm:grid-cols-2">{children}</div>
+      </Container>
+    </div>
+  );
+};
+
+const NavigationItems = ({ href, children }) => {
+  <Link
+    href={href}
+    className="group relative isolate -mx-6 bg-neutral-950 px-6 py-10 event:mx-px sm:mx-0 sm:py-16 sm:odd:pr-16 sm:even:mt-0 sm:even:border-1 sm:even:border-neutral-800 sm:even:pl-16"
+  >
+    {children}
+    <span className="absoulte inset-y-0 -z-10 w-screen bg-neutral-900 opacity-0 transition group-odd:right-0 group-even:left-0 group-hover:opacity-100" />
+  </Link>;
+};
+
+const Navigation = () => {
+  return (
+    <nav className="mt-px font-display text-5xl font-medium tracking-tight text-white">
+      <NavigationRow>
+        <NavigationItems href={"/work"}>Work</NavigationItems>
+        <NavigationItems href={"/aboutme"}>About Me</NavigationItems>
+      </NavigationRow>
+      <NavigationRow>
+        <NavigationItems href={"/contact"}>Contact</NavigationItems>
+      </NavigationRow>
+    </nav>
   );
 };
 
@@ -99,6 +133,32 @@ const RootLayoutInner = ({ children }) => {
             }}
           />
         </div>
+        <motion.div
+          layout
+          id={panelId}
+          style={{ height: expanded ? "auto" : ".5rem" }}
+          className="relative z-50 overflow-hidden bg-neutral-950 pt-2"
+          aria-hidden={expanded ? undefined : "true"}
+          inert={expanded ? undefined : ""}
+        >
+          <motion.div layout className="bg-neutral-800 ">
+            <div ref={navRef} className="bg-neutral-950 pb-16 pt-14">
+              <Header
+                invert
+                panelId={panelId}
+                icon={IoMdClose}
+                toggleRef={openRef}
+                expanded={expanded}
+                onToggle={() => {
+                  setExpanded((expanded) => !expanded);
+                  window.setTimeout(() => {
+                    openRef.current?.focus({ preventScroll: true });
+                  });
+                }}
+              />
+            </div>
+          </motion.div>
+        </motion.div>
       </header>
     </MotionConfig>
   );
