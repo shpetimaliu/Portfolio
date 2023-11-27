07a7dabe985543b99a395caf991fc9ce4312a883
Shpetim <shpetimmaliu@gmail.com>
2023-11-27 19:53:52 +0100

    up;

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,47 @@ import Container from "./Container";
 import { FadeIn } from "./FadeIn";
 import FooterNav from "./FooterNav";
 
+const ArrIcon = (props) => {
+  return (
+    <svg viewBox="0 0 16 6" aria-hidden="true" {...props}>
+      <path
+        fill="currentColor"
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M16 3 10 .5v2H0v1h10v2L16 3Z"
+      />
+    </svg>
+  );
+};
+
 const JustContact = () => {
   return (
     <form className="max-w-sm">
-      <h2 className="font-display text-sm font-medium tracking-wider text-neutral-700">
-        Just let an email
+      <h2 className="font-display text-sm font-semibold tracking-wider text-neutral-950">
+        Contact Directly
       </h2>
-      <p className="mt-4 text-sm text-neutral-700">loremipsum</p>
+      <p className="mt-4 text-sm text-neutral-700">
+        You can leave your phone number or email address, and I will contact you
+        as soon as possible
+      </p>
+      <div className="relative mt-6">
+        <input
+          type="email"
+          placeholder="Email address"
+          autoComplete="email"
+          aria-label="Email address"
+          className="block w-full rounded-2xl border border-neutral-300 bg-transparent py-4 pl-6 pr-20 text-base/6 text-neutral-950 ring-4 ring-transparent transition placeholder:text-neutral-500 focus:border-neutral-950 focus:outline-none focus:ring-neutral-950/5"
+        />
+        <div className="absolute inset-y-1 right-1 flex justify-end">
+          <button
+            type="submit"
+            aria-label="Submit"
+            className="flex aspect-square h-full items-center justify-center rounded-xl bg-neutral-950 text-white transition hover:bg-neutral-800"
+          >
+            <ArrIcon className="w-4" />
+          </button>
+        </div>
+      </div>
     </form>
   );
 };
@@ -19,6 +53,9 @@ function Footer() {
       <FadeIn>
         <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
           <FooterNav />
+          <div>
+            <JustContact />
+          </div>
         </div>
       </FadeIn>
     </Container>
